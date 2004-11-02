077749a95a48b4a69c28627b4abc2933299fd430
csalter <csalter>
2004-11-02 16:23:06 +0000

    Update copyright headers.

--- bundles/org.eclipse.wst.wsdl/src/org/eclipse/wst/wsdl/internal/extensibility/ExtensibilityElementFactoryRegistryReader.java
+++ bundles/org.eclipse.wst.wsdl/src/org/eclipse/wst/wsdl/internal/extensibility/ExtensibilityElementFactoryRegistryReader.java
@@ -1,9 +1,13 @@
-/*
- * Created on Apr 23, 2004
+/*******************************************************************************
+ * Copyright (c) 2001, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
- * @todo To change the template for this generated file go to Window -
- * Preferences - Java - Code Generation - Code and Comments
- */
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.wst.wsdl.internal.extensibility;
 import java.net.URL;
 
--- bundles/org.eclipse.wst.wsdl/src/org/eclipse/wst/wsdl/internal/impl/FaultImpl.java
+++ bundles/org.eclipse.wst.wsdl/src/org/eclipse/wst/wsdl/internal/impl/FaultImpl.java
@@ -1,4 +1,13 @@
-// $Id: FaultImpl.java,v 1.1 2004/11/01 23:07:29 csalter Exp $
+/*******************************************************************************
+ * Copyright (c) 2001, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.wst.wsdl.internal.impl;
 
 import org.eclipse.emf.ecore.EClass;
