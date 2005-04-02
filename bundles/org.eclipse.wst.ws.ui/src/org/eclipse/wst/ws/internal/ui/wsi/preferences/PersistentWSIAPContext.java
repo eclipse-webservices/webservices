98c48bd7b7e6b766ffb57ba1d9b6e4b73049ca64
cbrealey <cbrealey>
2005-04-02 04:28:15 +0000

    [90061] Move WS-I preference and context from jst.ws.ui to wst.us.ui.

---
+++ bundles/org.eclipse.wst.ws.ui/src/org/eclipse/wst/ws/internal/ui/wsi/preferences/PersistentWSIAPContext.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.wst.ws.internal.ui.wsi.preferences;
+
+import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.wst.ws.internal.ui.plugin.WSUIPlugin;
+
+
+
+public class PersistentWSIAPContext extends PersistentWSIContext
+{
+	private static final String NON_WSI_AP_COMPLIANCE = "nonWSIAPCompliance";
+
+public PersistentWSIAPContext () 
+{
+	super();
+	non_wsi_compliance = NON_WSI_AP_COMPLIANCE;
+	name = new QualifiedName(WSUIPlugin.ID , non_wsi_compliance);
+	wsiWarning_ = "WSI_AP_WARNING";
+	wsiError_ = "WSI_AP_ERROR";
+}
+
+public String getPersistentWSICompliance ()
+{
+	String property = getValueAsString(non_wsi_compliance);
+	// default to Ignore if no init has been done from ini file
+	if (property == null || property.equals("")) {
+		setValue( non_wsi_compliance, IGNORE_NON_WSI);
+		return IGNORE_NON_WSI;
+	}
+	else
+		return property;
+}
+}
