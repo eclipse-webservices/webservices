24d5787d1a159f820f54077c3e8547f9eb7e866f
kchong <kchong>
2005-04-14 15:11:21 +0000

    This commit was manufactured by cvs2svn to create tag 'v20050414_1113'.

--- bundles/org.eclipse.jst.ws.axis.creation.ui/src/org/eclipse/jst/ws/internal/axis/creation/ui/task/BUCheckAxisDeploymentDescriptors.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.jst.ws.internal.axis.creation.ui.task;
-
-import org.eclipse.jst.ws.internal.axis.consumption.ui.task.CheckAxisDeploymentDescriptorsTask;
-
-public class BUCheckAxisDeploymentDescriptors extends CheckAxisDeploymentDescriptorsTask 
-{
-}
