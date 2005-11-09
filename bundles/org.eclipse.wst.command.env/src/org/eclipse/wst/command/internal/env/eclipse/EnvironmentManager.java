5c9f47e13ca04ac003dfc98b357d5b2ff9581695
kchan <kchan>
2005-11-09 16:11:06 +0000

    [115124] Factory needed for IEnvironment.

---
+++ bundles/org.eclipse.wst.command.env/src/org/eclipse/wst/command/internal/env/eclipse/EnvironmentManager.java
@@ -0,0 +1,28 @@
+/***************************************************************************************************
+ * Copyright (c) 2003, 2005 IBM Corporation and others. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ **************************************************************************************************/
+package org.eclipse.wst.command.internal.env.eclipse;
+
+import org.eclipse.wst.command.internal.env.context.PersistentResourceContext;
+import org.eclipse.wst.common.environment.IStatusHandler;
+import org.eclipse.wst.common.environment.NullStatusHandler;
+
+public class EnvironmentManager
+{
+  /**
+   * Returns a new instance of an IEnvironment for the Eclipse non-GUI.
+   */
+  public static BaseEclipseEnvironment getNewEnvironment()
+  {
+    PersistentResourceContext context        = PersistentResourceContext.getInstance();
+    IStatusHandler            handler        = new NullStatusHandler();
+    EclipseEnvironment        environment    = new EclipseEnvironment( null, context, handler );
+    
+    return environment;  
+  }
+}
