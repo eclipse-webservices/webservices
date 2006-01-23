79aca0cf9a8a8a90b82b05738e7dd325aee3bcb8
lmandel <lmandel>
2006-01-23 22:20:52 +0000

    [117265] Added large workspace performance tests for WSDL validator. Updated performance plug-in to use manifest.mf.

---
+++ tests/org.eclipse.wst.wsdl.tests.performance/performance/org/eclipse/wst/wsdl/tests/performance/scalability/Validate1000WSDLsXSDDependsTestCase.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.wst.wsdl.tests.performance.scalability;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.wst.common.tests.performance.internal.scalability.RunValidatorTestCase;
+
+public class Validate1000WSDLsXSDDependsTestCase extends RunValidatorTestCase
+{
+	protected String getValidatorId()
+	{
+	    return "org.eclipse.wst.wsdl.ui.internal.validation.Validator";
+	}
+
+	protected String getBundleId()
+	{
+	    return "org.eclipse.wst.wsdl.tests.performance";
+	}
+	
+	protected void setUp() throws Exception
+	{
+	    IProject project = createProject("sp");
+	    String bundleId = getBundleId();
+	    for(int i = 1; i < 1000; i++)
+	    {
+	      copyFile(bundleId, "data/1000WSDLsXSDDepends/sample" + i +".wsdl", project);
+	    }
+	    super.setUp();
+	}
+
+	protected String getFilePath()
+	{
+	    return "data/1000WSDLsXSDDepends/sample0.xsd";
+	}
+	
+	public void testValidate1000WSDLsXSDDepends()
+	{
+	    try
+	    {
+	      super.execute();
+	    }
+	    catch (Throwable t)
+	    {
+	      fail(t.getMessage());
+	    }
+	}
+
+}
---
+++ tests/org.eclipse.wst.wsdl.tests.performance/performance/org/eclipse/wst/wsdl/tests/performance/scalability/Validate10WSDLsNoDependsTestCase.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.wst.wsdl.tests.performance.scalability;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.wst.common.tests.performance.internal.scalability.RunValidatorTestCase;
+
+public class Validate10WSDLsNoDependsTestCase extends RunValidatorTestCase
+{
+	protected String getValidatorId()
+	{
+	    return "org.eclipse.wst.wsdl.ui.internal.validation.Validator";
+	}
+
+	protected String getBundleId()
+	{
+	    return "org.eclipse.wst.wsdl.tests.performance";
+	}
+	
+	protected void setUp() throws Exception
+	{
+	    IProject project = createProject("sp");
+	    String bundleId = getBundleId();
+	    for(int i = 1; i < 10; i++)
+	    {
+	      copyFile(bundleId, "data/10WSDLsNoDepends/sample" + i +".wsdl", project);
+	    }
+	    super.setUp();
+	}
+
+	protected String getFilePath()
+	{
+	    return "data/10WSDLsNoDepends/sample0.xsd";
+	}
+	
+	public void testValidate10WSDLsNoDepends()
+	{
+	    try
+	    {
+	      super.execute();
+	    }
+	    catch (Throwable t)
+	    {
+	      fail(t.getMessage());
+	    }
+	}
+
+}
