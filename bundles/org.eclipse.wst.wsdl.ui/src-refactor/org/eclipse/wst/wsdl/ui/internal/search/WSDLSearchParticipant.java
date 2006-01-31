f90cdae8a77224144bc7452a819bff610d4040a8
csalter <csalter>
2006-01-31 06:27:13 +0000

    [125668] fix up some ugly participant initialization bugs

--- bundles/org.eclipse.wst.wsdl.ui/src-refactor/org/eclipse/wst/wsdl/ui/internal/search/WSDLSearchParticipant.java
+++ bundles/org.eclipse.wst.wsdl.ui/src-refactor/org/eclipse/wst/wsdl/ui/internal/search/WSDLSearchParticipant.java
@@ -30,12 +30,21 @@ public class WSDLSearchParticipant extends XMLSearchParticipant {
 	private static String ID = "org.eclipse.wst.wsdl.search.WSDLSearchParticipant";
 	private static String XSD_PARTICIPANNT_ID = "org.eclipse.wst.xsd.search.XSDSearchParticipant";
 
-
 	
-	public  boolean initialize(SearchPattern pattern, String[] contentTypes){
-		
-		super.initialize(pattern, contentTypes);
-		id = ID;
+	public WSDLSearchParticipant()
+	{
+	  super();
+      id = ID;
+	}
+	
+	public String[] getSupportedContentTypes()
+	{
+	  String[] result = { "org.eclipse.wst.wsdl.wsdlsource" };
+	  return result;
+	}
+	
+	public boolean isApplicable(SearchPattern pattern)
+	{
 		if(pattern instanceof XMLComponentSearchPattern ){
 			XMLComponentSearchPattern componentPattern = (XMLComponentSearchPattern)pattern;
 			String namespace = componentPattern.getMetaName().getNamespace();
@@ -45,9 +54,8 @@ public class WSDLSearchParticipant extends XMLSearchParticipant {
 			}
 		}
 		return false;
-	}
-
-
+	}	
+	
 	public ComponentSearchContributor getSearchContributor() {
 
 		return new WSDLSearchContributor();
