ad107d3bbd52272828f7429325c1c86fb1432685
kchan <kchan>
2006-02-22 18:18:20 +0000

    [124302] ProjectName2IProjectTransformer NPEs with null value.

--- bundles/org.eclipse.jst.ws.consumption.ui/src/org/eclipse/jst/ws/internal/consumption/ui/command/data/ProjectName2IProjectTransformer.java
+++ bundles/org.eclipse.jst.ws.consumption.ui/src/org/eclipse/jst/ws/internal/consumption/ui/command/data/ProjectName2IProjectTransformer.java
@@ -1,12 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2004 IBM Corporation and others.
+ * Copyright (c) 2004, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ * IBM Corporation - initial API and implementation
+ * yyyymmdd bug      Email and other contact information
+ * -------- -------- -----------------------------------------------------------
+ * 20060221   124302 rsinha@ca.ibm.com - Rupam Kuehner
  *******************************************************************************/
 package org.eclipse.jst.ws.internal.consumption.ui.command.data;
 
@@ -17,15 +20,21 @@ public class ProjectName2IProjectTransformer implements Transformer
 {
   public Object transform(Object value)
   {
-	String project     = (String)value;
-	int    slashIndex  = project.indexOf( '/' );
-	String projectName = project;
-	
-	if( slashIndex != -1 )
-	{
-	  projectName = project.substring( 0, slashIndex );
-	}
-	
-    return ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+    if (value != null)
+    {
+      String project = (String) value;
+      int slashIndex = project.indexOf('/');
+      String projectName = project;
+
+      if (slashIndex != -1)
+      {
+        projectName = project.substring(0, slashIndex);
+      }
+
+      return ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+    } else
+    {
+      return null;
+    }
   }
 }
