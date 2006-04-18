6618fae276744db582fb8050406c276a90ba8e15
csalter <csalter>
2006-04-18 04:28:12 +0000

    [136942] improve the 'extensions' view - fix some problems

--- bundles/org.eclipse.wst.wsdl.ui/src-asd-wsdl11/org/eclipse/wst/wsdl/ui/internal/properties/sections/W11ExtensionsSection.java
+++ bundles/org.eclipse.wst.wsdl.ui/src-asd-wsdl11/org/eclipse/wst/wsdl/ui/internal/properties/sections/W11ExtensionsSection.java
@@ -17,14 +17,20 @@ import java.util.List;
 import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.wst.wsdl.ExtensibilityElement;
 import org.eclipse.wst.wsdl.ExtensibleElement;
 import org.eclipse.wst.wsdl.ui.internal.Messages;
 import org.eclipse.wst.wsdl.ui.internal.WSDLEditorPlugin;
 import org.eclipse.wst.wsdl.ui.internal.commands.AddExtensionElementCommand;
+import org.eclipse.wst.wsdl.ui.internal.filter.ExtensiblityElementFilter;
 import org.eclipse.wst.xsd.ui.internal.common.commands.AddExtensionCommand;
 import org.eclipse.wst.xsd.ui.internal.common.properties.sections.AbstractExtensionsSection;
+import org.eclipse.wst.xsd.ui.internal.common.properties.sections.appinfo.AddExtensionsComponentDialog;
 import org.eclipse.wst.xsd.ui.internal.common.properties.sections.appinfo.DOMExtensionTreeContentProvider;
 import org.eclipse.wst.xsd.ui.internal.common.properties.sections.appinfo.DOMExtensionTreeLabelProvider;
 import org.eclipse.wst.xsd.ui.internal.common.properties.sections.appinfo.ExtensionsSchemasRegistry;
@@ -65,6 +71,52 @@ public class W11ExtensionsSection extends AbstractExtensionsSection
     return null;
   }
   
+  // TODO (cs) the AbstractExtensionsSection is polluted with XSD specic stuff
+  // need to clean that up!!
+  // TODO (cs) we should avoid referencing WSDL model objects ... go thru facade instead
+  public void setInput(IWorkbenchPart part, ISelection selection)
+  {
+    super.setInput(part, selection);
+    if (input instanceof Adapter)
+    {
+      input = ((Adapter)input).getTarget();
+    }
+    if (input instanceof ExtensibleElement)
+    {
+      isReadOnly = false;
+      //addButton.setEnabled(true);
+      //removeButton.setEnabled(true);      
+    }  
+    else
+    {
+      isReadOnly = true;      
+      //addButton.setEnabled(false);  
+      //removeButton.setEnabled(false); 
+    }  
+  }
+
+  protected AddExtensionsComponentDialog createAddExtensionsComponentDialog()
+  {
+    // IDEA (cs) we need a way to do all of this via the adapter
+    // so that we don't have any direct dependency on the WSDL1.1 model
+    // of course at the moment we call this class the W11ExtensionSections
+    // so that's not a problem.  In the future though we'll want to reuse this 
+    // class for WSDL 2.0.
+    //
+    if (input instanceof Adapter)
+    {
+      input = ((Adapter)input).getTarget();
+    }
+    if (input instanceof ExtensibleElement)
+    {    
+      ExtensibleElement extensibleElement = (ExtensibleElement)input;
+      AddExtensionsComponentDialog dialog = new AddExtensionsComponentDialog(composite.getShell(), getExtensionsSchemasRegistry());   
+      dialog.addElementsTableFilter(new AddExtensionsComponentDialogFilter(extensibleElement.getElement()));
+      return dialog;
+    }
+    return null;
+  }
+
   protected ExtensionsSchemasRegistry getExtensionsSchemasRegistry()
   {
     return WSDLEditorPlugin.getInstance().getExtensionsSchemasRegistry();
@@ -125,5 +177,33 @@ public class W11ExtensionsSection extends AbstractExtensionsSection
       }
       return super.getText(object);
     }        
+  }
+  /**
+   * This filter is to be used by the dialog invoked when addButton is pressed
+   */
+  private class AddExtensionsComponentDialogFilter extends ViewerFilter
+  {
+    private Element hostElement;
+
+    public AddExtensionsComponentDialogFilter(Element hostElement)
+    {
+      this.hostElement = hostElement;
+    }
+
+    public boolean select(Viewer viewer, Object parentElement, Object element)
+    {
+      if (element instanceof XSDElementDeclaration)
+      {
+        String namespace = ((XSDElementDeclaration) element).getTargetNamespace();
+        String name = ((XSDElementDeclaration) element).getName();
+        ExtensiblityElementFilter filter = (ExtensiblityElementFilter) WSDLEditorPlugin.getInstance().getExtensiblityElementFilterRegistry().getProperty(namespace, "");
+        if (filter != null)
+        {
+          return filter.isValidContext(hostElement, name);
+        }
+        return true;
+      }
+      return true;
+    }
   }
 }
