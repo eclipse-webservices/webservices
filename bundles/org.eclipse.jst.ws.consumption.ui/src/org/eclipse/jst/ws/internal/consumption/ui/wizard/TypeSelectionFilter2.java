a4bd0ed8c464798a42f290867d90a98e14f403da
cbrealey <cbrealey>
2006-06-06 19:59:31 +0000

    [105045] WSE - launch web service wizard - incorrect default

--- bundles/org.eclipse.jst.ws.consumption.ui/src/org/eclipse/jst/ws/internal/consumption/ui/wizard/TypeSelectionFilter2.java
+++ bundles/org.eclipse.jst.ws.consumption.ui/src/org/eclipse/jst/ws/internal/consumption/ui/wizard/TypeSelectionFilter2.java
@@ -6,7 +6,10 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ * IBM Corporation - initial API and implementation
+ * yyyymmdd bug      Email and other contact information
+ * -------- -------- -----------------------------------------------------------
+ * 20060606   105045 mahutch@ca.ibm.com - Mark Hutchinson          
  *******************************************************************************/
 
 package org.eclipse.jst.ws.internal.consumption.ui.wizard;
