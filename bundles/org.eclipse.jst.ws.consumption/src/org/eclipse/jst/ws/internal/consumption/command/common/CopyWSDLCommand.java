fcb42fb1047d3c0edce0ab803a15c82d58123041
kchan <kchan>
2006-12-14 04:21:39 +0000

    [165723] [165725] WSDL/XSD files not copied when import URL with relative imports are too long.  Validation error after copying.

--- bundles/org.eclipse.jst.ws.consumption/src/org/eclipse/jst/ws/internal/consumption/command/common/CopyWSDLCommand.java
+++ bundles/org.eclipse.jst.ws.consumption/src/org/eclipse/jst/ws/internal/consumption/command/common/CopyWSDLCommand.java
@@ -1,21 +1,22 @@
 /*******************************************************************************
- * Copyright (c) 2004 IBM Corporation and others.
+ * Copyright (c) 2004, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ * IBM Corporation - initial API and implementation
+ * yyyymmdd bug      Email and other contact information
+ * -------- -------- -----------------------------------------------------------
+ * 20061213   165723 kathy@ca.ibm.com - Kathy Chan
+ * 20061213   165725 kathy@ca.ibm.com - Kathy Chan
  *******************************************************************************/
 package org.eclipse.jst.ws.internal.consumption.command.common;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -37,18 +38,20 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.jst.ws.internal.common.ResourceUtils;
 import org.eclipse.jst.ws.internal.consumption.ConsumptionMessages;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.wst.command.internal.env.core.common.StatusUtils;
 import org.eclipse.wst.common.environment.IEnvironment;
 import org.eclipse.wst.common.environment.uri.URIException;
 import org.eclipse.wst.common.frameworks.datamodel.AbstractDataModelOperation;
-import org.eclipse.wst.common.uriresolver.internal.util.URIEncoder;
 import org.eclipse.wst.ws.internal.parser.discovery.NetUtils;
 import org.eclipse.wst.ws.internal.parser.wsil.WWWAuthenticationException;
 import org.eclipse.wst.ws.internal.parser.wsil.WebServicesParser;
@@ -78,24 +81,39 @@ public class CopyWSDLCommand extends AbstractDataModelOperation
   public IStatus execute( IProgressMonitor monitor, IAdaptable adaptable )
   {
     IEnvironment env = getEnvironment();
-    
+    CopyWSDLRunnable copyWSDLRunnable = new CopyWSDLRunnable(env);
     try
     {
-      ignoreList = new Vector();
-      if (def == null)
-        def = webServicesParser.getWSDLDefinition(wsdlURI);
-      copyWSDL(env, wsdlURI, getBaseURI(destinationURI), getLocalname(destinationURI), def);
+      ResourceUtils.getWorkspace().run(copyWSDLRunnable, null);
       return Status.OK_STATUS;
     }
-    catch (Throwable t)
+    catch (CoreException ce)
     {
-      t.printStackTrace();
-      IStatus status = StatusUtils.errorStatus( NLS.bind(ConsumptionMessages.MSG_ERROR_COPY_WSDL, new String[]{wsdlURI, destinationURI}), t);
-      env.getStatusHandler().reportError(status);
+        IStatus status = ce.getStatus();
+		env.getStatusHandler().reportError(status);
       return status;
     }
   }
 
+   
+  private class CopyWSDLRunnable implements IWorkspaceRunnable {
+  	
+  	private IEnvironment environment = null;
+  	
+  	protected CopyWSDLRunnable(IEnvironment env){
+  		environment = env;
+  	}
+  	public void run(IProgressMonitor pm) throws CoreException {
+  		ignoreList = new Vector();
+  		if (def == null)
+  			def = webServicesParser.getWSDLDefinition(wsdlURI);
+  		try {
+  			copyWSDL(environment, wsdlURI, getBaseURI(destinationURI), getLocalname(destinationURI), def);
+  		} catch (Throwable e) {
+  			throw new CoreException(StatusUtils.errorStatus( NLS.bind(ConsumptionMessages.MSG_ERROR_COPY_WSDL, new String[]{wsdlURI, destinationURI}), e));
+  		}
+  	}
+  }
   private void copyWSDL(IEnvironment env, String uri, String destURI, String destLocalname) throws WSDLException, IOException, WWWAuthenticationException, TransformerException, TransformerConfigurationException, URIException
   {
   	Definition definition;
@@ -110,10 +128,11 @@ public class CopyWSDLCommand extends AbstractDataModelOperation
 
   private void copyWSDL(IEnvironment env, String uri, String destURI, String destLocalname, Definition definition) throws WSDLException, IOException, WWWAuthenticationException, TransformerException, TransformerConfigurationException, URIException
   {
+	uri = normalize(uri);
 	if (!needToCopy(uri)) {
 	   	return;
 	}
-    
+	destURI = normalize(destURI);
     String baseURI = getBaseURI(uri);
     if (destLocalname == null || destLocalname.length() <= 0)
       destLocalname = getLocalname(uri);
@@ -228,10 +247,11 @@ public class CopyWSDLCommand extends AbstractDataModelOperation
 
   private void copyXMLSchema(IEnvironment env, String uri, String destURI) throws TransformerException, TransformerConfigurationException, IOException, URIException
   {
+	  uri = normalize(uri);
 	  if (!needToCopy(uri)) {
 	    	return;
 	    }
-	
+	  destURI = normalize(destURI);
     // load as a cached schema
     XSDSchema xsdSchema = XSDSchemaImpl.getSchemaForSchema(uri);
     // if schema is not cached, parse it
@@ -289,26 +309,35 @@ public class CopyWSDLCommand extends AbstractDataModelOperation
     }
   }
 
-  private boolean needToCopy (String uri) {
-	  String normalizedURI = normalize(uri);	
+  private boolean needToCopy (String normalizedURI) {	
 	  if (ignoreList.contains(normalizedURI))
 		  return false;
 	  ignoreList.add(normalizedURI);
 	  return true;
   }
   
+//normalize the uri to remove relative addressing "/.."
   private String normalize(String uri )
   {
-	  try {
-		  String encodedURI = URIEncoder.encode(uri,"UTF-8");
-		  URI normalizedURI = new URI(encodedURI);
-		  normalizedURI = normalizedURI.normalize();
-		  return normalizedURI.toString();
-	  } catch (URISyntaxException e) {
-		  return uri;
-	  } catch (UnsupportedEncodingException e) {
-		  return uri;
-	  }
+  	boolean normalized = false;
+  	while(!normalized){
+  	  int dir = uri.indexOf("/..");
+  	  if(dir == -1)
+  	  	normalized = true;
+  	  else{
+  	  	String first = uri.substring(0,dir);
+  	    String second = uri.substring(dir + 3);
+  	    int newIndex = first.lastIndexOf("/");
+  	    if(newIndex == -1){
+  	    	normalized = true;
+  	    } else {
+  	    	first = first.substring(0,newIndex);
+  	    	uri = first + second;
+  	    }
+  	  }
+  	}
+  	return uri;
+  
   }
 	  
   public void setWsdlURI(String wsdlURI)
