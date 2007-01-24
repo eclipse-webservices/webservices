a27691521e236eaf3717266c23130cb89f829e1c
lmandel <lmandel>
2007-01-24 21:59:17 +0000

    [154684] Fix for WSDL imports that use the Eclipse platform scheme.

--- bundles/org.eclipse.wst.wsdl.validation/src/org/eclipse/wst/wsdl/validation/internal/eclipse/URIResolverWrapper.java
+++ bundles/org.eclipse.wst.wsdl.validation/src/org/eclipse/wst/wsdl/validation/internal/eclipse/URIResolverWrapper.java
@@ -11,6 +11,12 @@
 
 package org.eclipse.wst.wsdl.validation.internal.eclipse;
 
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.wst.common.uriresolver.internal.provisional.URIResolver;
 import org.eclipse.wst.common.uriresolver.internal.provisional.URIResolverPlugin;
 import org.eclipse.wst.wsdl.validation.internal.resolver.IExtensibleURIResolver;
@@ -35,10 +41,11 @@ public class URIResolverWrapper implements IExtensibleURIResolver
   public void resolve(String baseLocation, String publicId, String systemId, IURIResolutionResult result)
   {
     URIResolver resolver = URIResolverPlugin.createResolver();
+    String resolvedSystemId = resolvePlatformURL(systemId);
     String location = null;
-    if (publicId != null || systemId != null)
+    if (publicId != null || resolvedSystemId != null)
     {  
-      location = resolver.resolve(baseLocation, publicId, systemId);
+      location = resolver.resolve(baseLocation, publicId, resolvedSystemId);
     }  
     
     if (location != null)
@@ -54,5 +61,35 @@ public class URIResolverWrapper implements IExtensibleURIResolver
         result.setPhysicalLocation(location);
       }
     }
+  }
+  
+  /**
+   * Resolve platform URLs to standard URLs. This method has the
+   * practical effect of transforming a URL such as
+   * platform:/test/test.wsdl
+   * into
+   * file:/c:/wtp/workspace/test/test.wsdl
+   * 
+   * @param url The URL that may contain the platform scheme.
+   * @return A resolved URL that does not contain the platform scheme.
+   */
+  protected String resolvePlatformURL(String url)
+  {
+	  String result = url;
+	  if(url != null && url.startsWith("platform:"))
+      {
+    	  try
+    	  {
+    		  IResource res = ResourcesPlugin.getWorkspace().getRoot().findMember(url);
+    		  URL tempurl = FileLocator.toFileURL(res.getLocation().toFile().toURL());
+    		  result = tempurl.toExternalForm();
+    	  }
+    	  catch(IOException e)
+    	  {
+    		  // Can't resolve using the FileLocator in this
+    		  // case so do nothing.
+    	  }
+      }
+	  return result;
   }
 }
