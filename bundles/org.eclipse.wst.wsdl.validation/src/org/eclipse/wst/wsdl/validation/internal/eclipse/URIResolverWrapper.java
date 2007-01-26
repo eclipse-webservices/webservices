411665a95bd5d0340d3b9faa633dd98251335fc4
lmandel <lmandel>
2007-01-26 03:52:48 +0000

    [154684] Secondary fix for problem with the original fix related to using the platform scheme on Linux and a problem with the test on Linux.

--- bundles/org.eclipse.wst.wsdl.validation/src/org/eclipse/wst/wsdl/validation/internal/eclipse/URIResolverWrapper.java
+++ bundles/org.eclipse.wst.wsdl.validation/src/org/eclipse/wst/wsdl/validation/internal/eclipse/URIResolverWrapper.java
@@ -11,11 +11,8 @@
 
 package org.eclipse.wst.wsdl.validation.internal.eclipse;
 
-import java.io.IOException;
 import java.net.URL;
 
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.wst.common.uriresolver.internal.provisional.URIResolver;
 import org.eclipse.wst.common.uriresolver.internal.provisional.URIResolverPlugin;
@@ -66,7 +63,7 @@ public class URIResolverWrapper implements IExtensibleURIResolver
   /**
    * Resolve platform URLs to standard URLs. This method has the
    * practical effect of transforming a URL such as
-   * platform:/test/test.wsdl
+   * platform:/resource/test/test.wsdl
    * into
    * file:/c:/wtp/workspace/test/test.wsdl
    * 
@@ -80,11 +77,10 @@ public class URIResolverWrapper implements IExtensibleURIResolver
       {
     	  try
     	  {
-    		  IResource res = ResourcesPlugin.getWorkspace().getRoot().findMember(url);
-    		  URL tempurl = FileLocator.toFileURL(res.getLocation().toFile().toURL());
-    		  result = tempurl.toExternalForm();
+    		  URL fileURL = FileLocator.toFileURL(new URL(url));
+    		  result = fileURL.toExternalForm();
     	  }
-    	  catch(IOException e)
+    	  catch(Exception e)
     	  {
     		  // Can't resolve using the FileLocator in this
     		  // case so do nothing.
