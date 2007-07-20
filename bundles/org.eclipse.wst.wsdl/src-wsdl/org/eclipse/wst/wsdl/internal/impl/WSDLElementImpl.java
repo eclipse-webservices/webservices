724eae3ab1d92694ed04528b518a072a32498741
vbaciu <vbaciu>
2007-07-20 16:14:25 +0000

    [197302] Adding output to message breaks graphical view if there is documentation

--- bundles/org.eclipse.wst.wsdl/src-wsdl/org/eclipse/wst/wsdl/internal/impl/WSDLElementImpl.java
+++ bundles/org.eclipse.wst.wsdl/src-wsdl/org/eclipse/wst/wsdl/internal/impl/WSDLElementImpl.java
@@ -738,8 +738,12 @@ public abstract class WSDLElementImpl extends EObjectImpl implements WSDLElement
 
     if (!isAttached)
     {
-      if (referencedElement == null && !eReference.isMany())
+      // If we're dealing with a documentation element we need to put it first in the list
+      
+      if (referencedElement == null && WSDLConstants.nodeType(childElement) == WSDLConstants.DOCUMENTATION && !eReference.isMany())
       {
+        // Here we find the first element node in the list, the documentation element needs to go before this element
+        
         for (Node child = adoptionParent.getFirstChild(); child != null; child = child.getNextSibling())
         {
           if (child.getNodeType() == Node.ELEMENT_NODE)
