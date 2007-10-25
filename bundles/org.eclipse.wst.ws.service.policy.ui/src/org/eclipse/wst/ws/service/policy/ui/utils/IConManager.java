cef82f85d534a50e3bb9f785ce96a53ab3b43569
pmoogk <pmoogk>
2007-10-25 20:13:24 +0000

    [196997] Initial code drop of service policy plugins

---
+++ bundles/org.eclipse.wst.ws.service.policy.ui/src/org/eclipse/wst/ws/service/policy/ui/utils/IConManager.java
@@ -0,0 +1,251 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ * yyyymmdd bug      Email and other contact information
+ * -------- -------- -----------------------------------------------------------
+ * 20071025   196997 pmoogk@ca.ibm.com - Peter Moogk, Initial coding.
+ *******************************************************************************/
+package org.eclipse.wst.ws.service.policy.ui.utils;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.resource.CompositeImageDescriptor;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.wst.ws.service.policy.ui.ServicePolicyActivatorUI;
+import org.osgi.framework.Bundle;
+
+public class IConManager
+{
+  private Map<String, Image> iconTable;
+  
+  public final String favorite = "fav";
+  public final String invalid  = "inv";
+  public final String warning  = "war";
+  public final String lock     = "loc";
+  
+  private String folderBaseURL; 
+  private String leafBaseURL;
+  private Image  favoriteImage;
+  private Image  invalidImage;
+  private Image  warningImage;
+  private Image  lockImage;
+  
+  private DecoratorDescriptor iconCreator;
+  
+  public IConManager()
+  {
+    iconTable = new HashMap<String, Image>();
+    addOverlayIcons();  
+    iconCreator = new DecoratorDescriptor();
+  }
+  
+  public String getFolderBaseUrl()
+  {
+    return folderBaseURL;  
+  }
+  
+  public String getLeafBaseUrl()
+  {
+    return leafBaseURL;  
+  }
+  
+  private void addOverlayIcons()
+  {
+    try
+    {
+      Bundle bundle      = ServicePolicyActivatorUI.getDefault().getBundle();
+      URL    folderUrl   = FileLocator.find( bundle, new Path( "icons/obj16/folder_obj.gif" ), null );
+      URL    leafUrl     = FileLocator.find( bundle, new Path( "icons/obj16/leaf_obj.gif" ), null );
+      URL    favoriteUrl = FileLocator.find( bundle, new Path( "icons/ovr16/favorite_ovr.gif" ), null );
+      URL    invalidUrl  = FileLocator.find( bundle, new Path( "icons/ovr16/invalidtype_ovr.gif" ), null );
+      URL    warningUrl  = FileLocator.find( bundle, new Path( "icons/ovr16/warning_ovr.gif" ), null );
+      URL    lockUrl     = FileLocator.find( bundle, new Path( "icons/ovr16/unmodifiable_ovr.gif" ), null );
+      
+      getIconImage( folderUrl );
+      getIconImage( leafUrl );
+      favoriteImage   = getIconImage( favoriteUrl );
+      invalidImage    = getIconImage( invalidUrl );
+      warningImage    = getIconImage( warningUrl );
+      lockImage       = getIconImage( lockUrl );
+      
+      folderBaseURL = folderUrl.toString();
+      leafBaseURL   = leafUrl.toString();
+    }
+    catch( Throwable exc )
+    {
+      ServicePolicyActivatorUI.logError( "Error reading icon overlays", exc );
+      exc.printStackTrace();
+    }
+  }
+  
+  public boolean hasChanged( String[] string1, String[] string2 )
+  {
+    if( string1 == null || string2 == null || string1.length != string2.length ) return true;
+    
+    for( int index = 0; index < string1.length; index++ )
+    {
+      String value1 = string1[index];
+      String value2 = string2[index];
+      
+      // We can do pointer comparison since we are using constant Strings.
+      if( value1 != value2 )
+      {
+        return true;
+      }
+    }
+    
+    return false;
+  }
+  
+  public Image getIconOverlay( String baseUrl, String[] overlays )
+  {
+    String imageUrl  = getImageURL( baseUrl, overlays ); 
+    Image  result    = (Image)iconTable.get( imageUrl );
+    
+    if( result == null )
+    {
+      Image baseImage = (Image)iconTable.get( baseUrl );
+      
+      if( baseImage == null )
+      {
+        // We need to load this baseImage
+        try
+        {
+          baseImage = getIconImage( new URL( baseUrl ) );
+        }
+        catch( Exception exc )
+        {
+          ServicePolicyActivatorUI.logError( "Error loading image from:" + baseUrl, exc);
+        }
+      }
+      
+      iconCreator.setBaseImage( baseImage, overlays );
+      
+      result = iconCreator.createImage();
+      iconTable.put( imageUrl, result );
+    }
+    
+    return result;
+  }
+  
+  private boolean hasOverlay( String[] overlays, String value )
+  {
+    boolean result = false;
+       
+    for( String overlay : overlays )
+    {
+      // Note: We are using string pointer comparison here since, the strings
+      // should only be the constants defined above.
+      if( overlay == value )
+      {
+        result = true;
+        break;
+      }     
+    }
+    
+    return result;
+  }
+  
+  private String getImageURL( String baseUrl, String[] overlays )
+  {
+    String url = baseUrl;
+    
+    for( String value : overlays )
+    {
+      if( value != null )
+      {
+        url = url + ":" + value;
+      }
+    }
+    
+    return url;
+  }
+  
+  public Image getIconImage( URL url )
+  {
+    String urlString = url.toString();
+    Image  image     = iconTable.get( urlString );
+    
+    if( image == null )
+    {
+      ImageDescriptor imageDesc = ImageDescriptor.createFromURL(url);
+      
+      image = imageDesc.createImage();
+      iconTable.put( urlString, image );
+    }
+    
+    return image;
+  }
+  
+  public void dispose()
+  {
+    for( Image image : iconTable.values() )
+    {
+      image.dispose();
+    }
+  }
+  
+  private class DecoratorDescriptor extends CompositeImageDescriptor
+  {
+    private Image    baseImage;
+    private Point    size;
+    private String[] overlays;
+    
+    protected void drawCompositeImage(int width, int height)
+    {
+      drawImage( baseImage.getImageData(), 0, 0);
+      
+      for( int index = 0; index < overlays.length; index++ )
+      {
+        if( hasOverlay(overlays, favorite) )
+        {
+          drawImage( favoriteImage.getImageData(), size.x - favoriteImage.getBounds().width, 0 );
+        }
+        
+        if( hasOverlay( overlays, warning ) )
+        {
+          drawImage( warningImage.getImageData(), 0, size.y - warningImage.getBounds().height );       
+        }
+        
+        if( hasOverlay( overlays, invalid ) )
+        {
+          drawImage( invalidImage.getImageData(), 0, size.y - invalidImage.getBounds().height );       
+        }
+        
+        if( hasOverlay( overlays, lock ) )
+        {
+          drawImage( lockImage.getImageData(), 
+                     size.x - lockImage.getBounds().width, 
+                     size.y - lockImage.getBounds().height );       
+        }
+      }
+    }
+
+    public void setBaseImage( Image image, String[] overlays )
+    { 
+      Rectangle bounds = image.getBounds();
+      
+      this.baseImage = image;
+      this.size      = new Point( bounds.width, bounds.height );
+      this.overlays  = overlays;
+    }
+    
+    protected Point getSize()
+    {
+      return size;
+    }
+  }
+}
