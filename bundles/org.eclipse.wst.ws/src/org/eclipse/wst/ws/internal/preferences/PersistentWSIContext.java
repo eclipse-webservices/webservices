005deeadcbf678b860255f18032014d01343252c
pmoogk <pmoogk>
2007-11-08 21:19:07 +0000

    [209267] Null pointer in Web services bottom up scenario

--- bundles/org.eclipse.wst.ws/src/org/eclipse/wst/ws/internal/preferences/PersistentWSIContext.java
+++ bundles/org.eclipse.wst.ws/src/org/eclipse/wst/ws/internal/preferences/PersistentWSIContext.java
@@ -1,12 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ * IBM Corporation - initial API and implementation
+ * yyyymmdd bug      Email and other contact information
+ * -------- -------- -----------------------------------------------------------
+ * 20071108   209267 pmoogk@ca.ibm.com - Peter Moogk
  *******************************************************************************/
 
 package org.eclipse.wst.ws.internal.preferences;
@@ -162,7 +165,7 @@ private String getProjectPersistentProperty(IProject project)
 
   protected String getServicePolicyId()
   {
-    return "";
+    return "org.eclipse.wst.ws.service.policy.ui.servicepols.wsiprofilecomp.wsissbp";
   }
   
   public QualifiedName getName()
