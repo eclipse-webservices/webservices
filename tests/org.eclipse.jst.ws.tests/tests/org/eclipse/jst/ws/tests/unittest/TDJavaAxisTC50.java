574efdaa0b71a2e311c4f51aad6750adc98aec00
kchan <kchan>
2008-02-12 14:37:40 +0000

    [217346] Better logging in JUnit test.

--- tests/org.eclipse.jst.ws.tests/tests/org/eclipse/jst/ws/tests/unittest/TDJavaAxisTC50.java
+++ tests/org.eclipse.jst.ws.tests/tests/org/eclipse/jst/ws/tests/unittest/TDJavaAxisTC50.java
@@ -150,8 +150,7 @@ public class TDJavaAxisTC50 extends WSWizardTomcat50Test {
 				System.out.println("Error message for report #"+i+": "+s[i].getMessage());
 			}
 		} 
-		assertTrue(s.length == 0);
-		System.out.println(" TD # of Error reports = "+s.length);
+		assertEquals("TD Unexpected number of client error reports", 0, s.length);
 		
 	}
 	
