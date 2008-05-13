a8abc40fea62099c3c4618672dcfa8b53d797e3a
kchan <kchan>
2008-05-13 14:46:57 +0000

    [231161] Accessibility problem with services policy wizard.

--- bundles/org.eclipse.wst.ws.service.policy.ui/src/org/eclipse/wst/ws/internal/service/policy/ui/ScrolledPageContent.java
+++ bundles/org.eclipse.wst.ws.service.policy.ui/src/org/eclipse/wst/ws/internal/service/policy/ui/ScrolledPageContent.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,7 @@
  * yyyymmdd bug      Email and other contact information
  * -------- -------- -----------------------------------------------------------
  * 20071120   209858 ericdp@ca.ibm.com - Eric Peters, Enhancing service policy framework and UI
+ * 20080512   231161 pmoogk@ca.ibm.com - Peter Moogk, Fix tab key accessiblity problem.
  *******************************************************************************/
 package org.eclipse.wst.ws.internal.service.policy.ui;
 
@@ -56,7 +57,10 @@ public class ScrolledPageContent extends SharedScrolledComposite {
 	}
 
 	public void adaptChild(Control childControl) {
-		fToolkit.adapt(childControl, true, true);
+	  // Need to have the track keyboard parameter false, since a true
+	  // value causes Composite controls to eat tab keys, which in turn
+	  // causes the whole preference page to become inaccessible.
+		fToolkit.adapt(childControl, true, false);
 	}
 	
 	public Composite getBody() {
