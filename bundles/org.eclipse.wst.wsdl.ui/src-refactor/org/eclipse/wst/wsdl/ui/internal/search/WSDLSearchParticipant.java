bb8788266fd2f721ff48098c684fe022664f3458
vbaciu <vbaciu>
2008-05-28 15:41:13 +0000

    [232618] search and refactoring support for xsd not finding references in wsdl

--- bundles/org.eclipse.wst.wsdl.ui/src-refactor/org/eclipse/wst/wsdl/ui/internal/search/WSDLSearchParticipant.java
+++ bundles/org.eclipse.wst.wsdl.ui/src-refactor/org/eclipse/wst/wsdl/ui/internal/search/WSDLSearchParticipant.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * Copyright (c) 2001, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -90,7 +90,7 @@ public class WSDLSearchParticipant extends XMLSearchParticipant {
 					}
 				}
 			}
-			componentPattern.setChildren((XMLSearchPattern[]) patterns.toArray(new XMLSearchPattern[patterns.size()]));
+			componentPattern.addChildren(this, (XMLSearchPattern[]) patterns.toArray(new XMLSearchPattern[patterns.size()]));
 			
 		}
 		else if(pattern instanceof XMLComponentReferencePattern){
@@ -115,7 +115,7 @@ public class WSDLSearchParticipant extends XMLSearchParticipant {
 					}
 				}
 			}
-			componentPattern.setChildren((XMLSearchPattern[]) patterns.toArray(new XMLSearchPattern[patterns.size()]));
+			componentPattern.addChildren(this, (XMLSearchPattern[]) patterns.toArray(new XMLSearchPattern[patterns.size()]));
 			
 		}
 		
