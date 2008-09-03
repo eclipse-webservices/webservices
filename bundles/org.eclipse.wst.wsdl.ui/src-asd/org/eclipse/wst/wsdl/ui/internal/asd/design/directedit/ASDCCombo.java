b5b52a74a656b75f73fdc10e8abccaff4e3c84b0
amywu <amywu>
2008-09-03 16:50:21 +0000

    [245594] new and browse for type dialog in wsdl editor does not work in design view

--- bundles/org.eclipse.wst.wsdl.ui/src-asd/org/eclipse/wst/wsdl/ui/internal/asd/design/directedit/ASDCCombo.java
+++ bundles/org.eclipse.wst.wsdl.ui/src-asd/org/eclipse/wst/wsdl/ui/internal/asd/design/directedit/ASDCCombo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -917,9 +917,7 @@ void listEvent (Event event) {
 			e.time = event.time;
 			e.stateMask = event.stateMask;
 			e.doit = event.doit;
-			if (!isDropped()) {
-				notifyListeners (SWT.Selection, e);
-			}
+			notifyListeners (SWT.Selection, e);
 			event.doit = e.doit;
 			break;
 		}
