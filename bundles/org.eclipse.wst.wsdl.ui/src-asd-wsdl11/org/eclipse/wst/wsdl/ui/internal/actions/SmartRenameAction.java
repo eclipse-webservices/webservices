794c702c864a113930b41d87a1be776b6b05abd3
amywu <amywu>
2008-09-03 22:42:41 +0000

    [216631] npe when rename operation in design view

--- bundles/org.eclipse.wst.wsdl.ui/src-asd-wsdl11/org/eclipse/wst/wsdl/ui/internal/actions/SmartRenameAction.java
+++ bundles/org.eclipse.wst.wsdl.ui/src-asd-wsdl11/org/eclipse/wst/wsdl/ui/internal/actions/SmartRenameAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2007 IBM Corporation and others.
+ * Copyright (c) 2001, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -128,7 +128,7 @@ public class SmartRenameAction extends BaseNodeAction implements Runnable {
 					Binding binding = (Binding) iterator.next();
 					
 					// Find only Bindings which reference the modified PortType-->Operation
-					if (binding.getPortType().equals(portType)) {
+					if (portType.equals(binding.getPortType())) {
 						String operationInputName = null;
 						String operationOutputName = null;
 						if(operation.getInput() != null) {
