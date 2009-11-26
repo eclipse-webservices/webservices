46fdff8a29862512749f3d1e5f90b89b298d86b4
kchong <kchong>
2009-11-26 22:06:37 +0000

    [242615] Use project creation operations to create projects

--- bundles/org.eclipse.jst.ws.consumption/src/org/eclipse/jst/ws/internal/consumption/command/common/CreateFacetedProjectCommand.java
+++ bundles/org.eclipse.jst.ws.consumption/src/org/eclipse/jst/ws/internal/consumption/command/common/CreateFacetedProjectCommand.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -21,6 +21,7 @@
  * 20080305   220371 kathy@ca.ibm.com - Kathy Chan - reverting previous change to 220371
  * 20080326   220371 kathy@ca.ibm.com - Kathy Chan - re-applying 220371
  * 20081001   243869 ericdp@ca.ibm.com - Eric D. Peters, Web Service tools allowing mixed J2EE levels
+ * 20091123   242615 mahutch@ca.ibm.com - Mark Hutchinson, Use project creation operations to create projects
  *******************************************************************************/
 
 package org.eclipse.jst.ws.internal.consumption.command.common;
@@ -29,6 +30,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
@@ -36,6 +38,12 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jem.util.emf.workbench.ProjectUtilities;
+import org.eclipse.jst.j2ee.internal.ejb.project.operations.EjbFacetProjectCreationDataModelProvider;
+import org.eclipse.jst.j2ee.internal.plugin.IJ2EEModuleConstants;
+import org.eclipse.jst.j2ee.internal.project.facet.UtilityFacetInstallDataModelProvider;
+import org.eclipse.jst.j2ee.internal.web.archive.operations.WebFacetProjectCreationDataModelProvider;
+import org.eclipse.jst.j2ee.project.facet.AppClientFacetInstallDataModelProvider;
+import org.eclipse.jst.j2ee.project.facet.IJ2EEFacetProjectCreationDataModelProperties;
 import org.eclipse.jst.server.core.FacetUtil;
 import org.eclipse.jst.ws.internal.common.J2EEUtils;
 import org.eclipse.jst.ws.internal.common.ServerUtils;
@@ -46,8 +54,13 @@ import org.eclipse.jst.ws.internal.consumption.common.FacetUtils;
 import org.eclipse.jst.ws.internal.consumption.common.RequiredFacetVersion;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.wst.command.internal.env.core.common.StatusUtils;
+import org.eclipse.wst.common.componentcore.datamodel.properties.IFacetDataModelProperties;
+import org.eclipse.wst.common.componentcore.datamodel.properties.IFacetProjectCreationDataModelProperties;
+import org.eclipse.wst.common.componentcore.datamodel.properties.IFacetProjectCreationDataModelProperties.FacetDataModelMap;
 import org.eclipse.wst.common.componentcore.internal.util.IModuleConstants;
 import org.eclipse.wst.common.frameworks.datamodel.AbstractDataModelOperation;
+import org.eclipse.wst.common.frameworks.datamodel.DataModelFactory;
+import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
 import org.eclipse.wst.common.project.facet.core.IFacetedProject;
 import org.eclipse.wst.common.project.facet.core.IFacetedProjectTemplate;
 import org.eclipse.wst.common.project.facet.core.IProjectFacet;
@@ -86,112 +99,184 @@ public class CreateFacetedProjectCommand extends AbstractDataModelOperation
   private org.eclipse.wst.common.project.facet.core.runtime.IRuntime facetRuntime;
   
   
+  private boolean containsWebFacet(Set facetVersions) {
+	  for (Iterator iter = facetVersions.iterator(); iter.hasNext();) {
+		  IProjectFacetVersion requiredFacetVersion = (IProjectFacetVersion) iter.next();
+		  if (requiredFacetVersion.getProjectFacet().getId().equals(IModuleConstants.JST_WEB_MODULE)) {
+			  return true;
+		  }
+	  }
+	  return false;
+  }
+  
+  //return the project creation data model for the templateId or null if no data model found
+  private IDataModel getDataModelForTemplate(Set requiredFixedFacets) {
+	  IDataModel dm = null;	  
+	  if (templateId == null) 
+		  return null;
+	  else if (templateId.equals(IJ2EEModuleConstants.JST_EJB_TEMPLATE)) {
+		  dm = DataModelFactory.createDataModel(new EjbFacetProjectCreationDataModelProvider());  
+		  dm.setProperty(IJ2EEFacetProjectCreationDataModelProperties.FACET_PROJECT_NAME, projectName);
+		  dm.setBooleanProperty(IJ2EEFacetProjectCreationDataModelProperties.ADD_TO_EAR, false);
+		  dm.setProperty(IJ2EEFacetProjectCreationDataModelProperties.FACET_RUNTIME, facetRuntime);
+	  }
+	  else if (templateId.equals(IJ2EEModuleConstants.JST_APPCLIENT_TEMPLATE)) {
+		  dm = DataModelFactory.createDataModel(new AppClientFacetInstallDataModelProvider());
+		  dm.setProperty(IJ2EEFacetProjectCreationDataModelProperties.FACET_PROJECT_NAME, projectName);
+		  dm.setProperty(IJ2EEFacetProjectCreationDataModelProperties.FACET_RUNTIME, facetRuntime);		  
+	  }
+	  else if (templateId.equals(IJ2EEModuleConstants.JST_UTILITY_TEMPLATE)) {
+		  dm = DataModelFactory.createDataModel(new UtilityFacetInstallDataModelProvider());
+		  dm.setProperty(IJ2EEFacetProjectCreationDataModelProperties.FACET_PROJECT_NAME, projectName);
+		  dm.setProperty(IJ2EEFacetProjectCreationDataModelProperties.FACET_RUNTIME, facetRuntime);
+	  }
+	  else if (templateId.equals(IJ2EEModuleConstants.JST_WEB_TEMPLATE) || containsWebFacet(requiredFixedFacets)) {
+		  //we want any project with the web template ID, or that contains a jst.web facet to be created with this data model
+		  dm = DataModelFactory.createDataModel(new WebFacetProjectCreationDataModelProvider());
+		  dm.setProperty(IJ2EEFacetProjectCreationDataModelProperties.FACET_PROJECT_NAME, projectName);
+		  dm.setBooleanProperty(IJ2EEFacetProjectCreationDataModelProperties.ADD_TO_EAR, false);
+		  dm.setProperty(IJ2EEFacetProjectCreationDataModelProperties.FACET_RUNTIME, facetRuntime);	  		  
+	  }
+	  return dm;
+  }
+  
   public IStatus execute(IProgressMonitor monitor, IAdaptable info)
   {
-    IStatus status = Status.OK_STATUS;
-    
-    // check if data ready
-    status = checkDataReady();
-    if (status.getSeverity()==Status.ERROR)
-    {
-      return status;
-    }
+	  IStatus status = Status.OK_STATUS;
 
-    IProject project = ProjectUtilities.getProject(projectName);
-    if (!project.exists())
-    {
-      try
-      {
-        status = FacetUtils.createNewFacetedProject(projectName);
-        if (status.getSeverity() == IStatus.ERROR)
-        {
-          return status;
-        }
-        
-        IProject createdProject = ProjectUtilities.getProject(projectName);
-        IFacetedProject fproject = ProjectFacetsManager.create(createdProject);
-        
-        //Get the facet versions to install. 
-        Set facetsToAdd = getFacetsToAdd();
-        
-        //Install the facet versions
-        status = FacetUtils.addFacetsToProject(fproject, facetsToAdd);
-        if (status.getSeverity() == IStatus.ERROR)
-        {
-          return status;
-        }        
-        
-        //Set the installed facet versions as fixed.
-        Set newFacetVersions = fproject.getProjectFacets();
-        Set fixedFacets = new HashSet();
-        for (Iterator iter = newFacetVersions.iterator(); iter.hasNext();) {
-            IProjectFacetVersion facetVersion = (IProjectFacetVersion) iter.next();
-            fixedFacets.add(facetVersion.getProjectFacet());
-        }
-        status = FacetUtils.setFixedFacetsOnProject(fproject, fixedFacets);
-        if (status.getSeverity() == IStatus.ERROR)
-        {
-          return status;
-        }                
-        
-        
-        //Set the runtime        
-        if (facetRuntime != null)
-        {
-          status = FacetUtils.setFacetRuntimeOnProject(fproject, facetRuntime);
-        }
+	  // check if data ready
+	  status = checkDataReady();
+	  if (status.getSeverity()==Status.ERROR)
+	  {
+		  return status;
+	  }
 
-        // add facets required by Web service runtime
-        if (requiredFacetVersions.length != 0) {
-        	status = FacetUtils.addRequiredFacetsToProject(project, requiredFacetVersions, monitor);
-        	if (status.getSeverity() == Status.ERROR)
-        	{
-        		return status;
-        	}      
-        }
-             
-        if (facetRuntime != null)
-        {
-        	// add the default facets that's not in conflict with the existing facets
-        	Set projectFacetVersionSet = fproject.getProjectFacets();
-        	Set projectFacetSet = new HashSet();
-        	// get the project facet from the project facet version we calculated
-        	for (Iterator iter = projectFacetVersionSet.iterator(); iter.hasNext();) {
-        		IProjectFacetVersion pfv = (IProjectFacetVersion) iter.next();
-        		projectFacetSet.add(pfv.getProjectFacet());
-        	}
-        	try {
-        		Set defaultProjectFacetVersionSet = facetRuntime.getDefaultFacets(projectFacetSet);
-        		// The returned defaultFacetSet contains the original projectFacetSet passed into getDefaultFacets
-        		// plus any default facets that are not in conflict with the original projectFacetSet.
-        		// Add to facetsToAdd if the the default facet is not in the original set.
-        		Set defaultFacetsToAdd = new HashSet();
-        		for (Iterator iter = defaultProjectFacetVersionSet.iterator(); iter.hasNext();) {
-        			IProjectFacetVersion defaultProjFacetVersion = (IProjectFacetVersion) iter.next();
-        			if( ! projectFacetSet.contains( defaultProjFacetVersion.getProjectFacet() ) )
-        			{
-        				defaultFacetsToAdd.add(defaultProjFacetVersion);
-        			}
-        		}
-        		if (!defaultFacetsToAdd.isEmpty()) {
-        			status = FacetUtils.addFacetsToProject(fproject, defaultFacetsToAdd);
-        			if (status.getSeverity() == IStatus.ERROR)
-        			{
-        				return status;
-        			}    
-        		}
-        	} catch (CoreException e) {
-        		// If there's any exception when trying to get the default facet, just ignore the default facet
-        		// and return the original facetsToAdd.
-        	}
-        	// end of adding default facets
-        }
-      } catch (CoreException ce)
-      {
-        return StatusUtils.errorStatus(NLS.bind(ConsumptionMessages.MSG_ERROR_PROJECT_CREATION, new String[] { projectName }));
-      }
-    }
-    return status;
+	  IProject project = ProjectUtilities.getProject(projectName);
+	  if (!project.exists())
+	  {
+		  try{
+			  IProject createdProject;
+			  IFacetedProject fproject;
+			  Set facetsToAdd = getFacetsToAdd();
+
+			  IDataModel dm = getDataModelForTemplate(facetsToAdd);
+			  if (dm != null) {
+				  //we found a project creation data model for this template id.  use it to create the project
+
+				  Set missingFacets = new HashSet();
+
+				  //here we look at the facet versions in facetsToAdd and set the version on the data model to match
+				  //if no facet data model is found we add it to the missingFacets collection to add later  
+				  if (dm.isProperty(IFacetProjectCreationDataModelProperties.FACET_DM_MAP)) {
+					  for (Iterator iter = facetsToAdd.iterator(); iter.hasNext();) {
+						  IProjectFacetVersion requiredFacetVersion = (IProjectFacetVersion) iter.next();
+						  final FacetDataModelMap map = (FacetDataModelMap) dm.getProperty(IFacetProjectCreationDataModelProperties.FACET_DM_MAP);
+						  IDataModel facetDM = map.getFacetDataModel(requiredFacetVersion.getProjectFacet().getId());
+						  if (facetDM != null) {
+							  facetDM.setStringProperty(IFacetDataModelProperties.FACET_VERSION_STR,  requiredFacetVersion.getVersionString());
+						  } else {
+							  missingFacets.add(requiredFacetVersion);
+						  }
+					  }
+				  }
+
+				  dm.getDefaultOperation().execute(null, null);
+				  createdProject = ProjectUtilities.getProject(projectName);
+				  fproject = ProjectFacetsManager.create(createdProject);
+
+				  //add any facets that were not added by the project creation data model
+				  if (missingFacets.size() > 0) {
+					  status = FacetUtils.addFacetsToProject(fproject, missingFacets);
+					  if (status.getSeverity() == IStatus.ERROR) {
+						  return status;
+					  }
+				  }
+			  }
+			  else {
+				  //did not find a project creation data model.  create the project and install facets manually
+				  status = FacetUtils.createNewFacetedProject(projectName);
+				  if (status.getSeverity() == IStatus.ERROR) {
+					  return status;
+				  }
+
+				  createdProject = ProjectUtilities.getProject(projectName);
+				  fproject = ProjectFacetsManager.create(createdProject);
+
+				  //Install the facet versions
+				  status = FacetUtils.addFacetsToProject(fproject, facetsToAdd);
+				  if (status.getSeverity() == IStatus.ERROR) {
+					  return status;
+				  }
+				  //Set the installed facet versions as fixed.
+				  Set newFacetVersions = fproject.getProjectFacets();
+				  Set fixedFacets = new HashSet();
+				  for (Iterator iter = newFacetVersions.iterator(); iter.hasNext();) {
+					  IProjectFacetVersion facetVersion = (IProjectFacetVersion) iter.next();
+					  fixedFacets.add(facetVersion.getProjectFacet());
+				  }
+				  status = FacetUtils.setFixedFacetsOnProject(fproject, fixedFacets);
+				  if (status.getSeverity() == IStatus.ERROR) {
+					  return status;
+				  }
+			  }
+
+			  //Set the runtime        
+			  if (facetRuntime != null) {
+				  status = FacetUtils.setFacetRuntimeOnProject(fproject, facetRuntime);
+			  }
+
+			  // add facets required by Web service runtime
+			  if (requiredFacetVersions.length != 0) {
+				  status = FacetUtils.addRequiredFacetsToProject(project, requiredFacetVersions, monitor);
+				  if (status.getSeverity() == Status.ERROR) {
+					  return status;
+				  }
+			  }
+
+			  if (facetRuntime != null) {
+				  // add the default facets that's not in conflict with the existing facets
+				  Set projectFacetVersionSet = fproject.getProjectFacets();
+				  Set projectFacetSet = new HashSet();
+				  // get the project facet from the project facet version we calculated
+				  for (Iterator iter = projectFacetVersionSet.iterator(); iter.hasNext();) {
+					  IProjectFacetVersion pfv = (IProjectFacetVersion) iter.next();
+					  projectFacetSet.add(pfv.getProjectFacet());
+				  }
+				  try {
+					  Set defaultProjectFacetVersionSet = facetRuntime.getDefaultFacets(projectFacetSet);
+					  // The returned defaultFacetSet contains the original projectFacetSet passed into getDefaultFacets
+					  // plus any default facets that are not in conflict with the original projectFacetSet.
+					  // Add to facetsToAdd if the the default facet is not in the original set.
+					  Set defaultFacetsToAdd = new HashSet();
+					  for (Iterator iter = defaultProjectFacetVersionSet.iterator(); iter.hasNext();) {
+						  IProjectFacetVersion defaultProjFacetVersion = (IProjectFacetVersion) iter.next();
+						  if( ! projectFacetSet.contains( defaultProjFacetVersion.getProjectFacet() ) )
+						  {
+							  defaultFacetsToAdd.add(defaultProjFacetVersion);
+						  }
+					  }
+					  if (!defaultFacetsToAdd.isEmpty()) {
+						  status = FacetUtils.addFacetsToProject(fproject, defaultFacetsToAdd);
+						  if (status.getSeverity() == IStatus.ERROR) {
+							  return status;
+						  }
+					  }
+				  } catch (CoreException e) {
+					  // If there's any exception when trying to get the default facet, just ignore the default facet
+					  // and return the original facetsToAdd.
+				  }
+			  }
+		  }
+		  catch (CoreException ce) {
+			  return StatusUtils.errorStatus(NLS.bind(ConsumptionMessages.MSG_ERROR_PROJECT_CREATION, new String[] { projectName }));
+		  }
+		  catch (ExecutionException e) {
+			  return StatusUtils.errorStatus(NLS.bind(ConsumptionMessages.MSG_ERROR_PROJECT_CREATION, new String[] { projectName }));
+		  }
+
+	  }
+
+	  return status;
   }
     
   private IStatus checkDataReady()
