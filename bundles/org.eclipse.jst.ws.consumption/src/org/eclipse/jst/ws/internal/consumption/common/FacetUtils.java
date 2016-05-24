c307c1726465f91a9b15418707794fdb40d795d3
kchong <kchong@ca.ibm.com>
2016-05-24 10:22:16 -0400

    [494208] Add Java 8 facet version to JavaFacetUtil
    Change-Id: Ibc2f5d834a2e436d64af59f1607451cb423f7e81

--- bundles/org.eclipse.jst.ws.consumption/src/org/eclipse/jst/ws/internal/consumption/common/FacetUtils.java
+++ bundles/org.eclipse.jst.ws.consumption/src/org/eclipse/jst/ws/internal/consumption/common/FacetUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2012 IBM Corporation and others.
+ * Copyright (c) 2007, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1027,6 +1027,10 @@ public class FacetUtils
     else if (jdkComplianceLevel.equals("1.7"))
     {
       javaFacetVersion = JavaFacet.VERSION_1_7;
+    }
+    else if (jdkComplianceLevel.equals("1.8"))
+    {
+      javaFacetVersion = JavaFacet.VERSION_1_8;
     }
     else
     {
