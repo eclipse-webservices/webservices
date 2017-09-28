111ea3ae798563b2f7fa098024278b455c2b135e
Keith Chong <kchong@ca.ibm.com>
2017-09-28 16:18:42 -0400

    [525337] Update Web Services to support Java 9
    Change-Id: I48548ab6e231d129497998196cc542e544d2ce6a

--- bundles/org.eclipse.jst.ws.consumption/src/org/eclipse/jst/ws/internal/consumption/common/FacetUtils.java
+++ bundles/org.eclipse.jst.ws.consumption/src/org/eclipse/jst/ws/internal/consumption/common/FacetUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2016 IBM Corporation and others.
+ * Copyright (c) 2007, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1031,6 +1031,10 @@ public class FacetUtils
     else if (jdkComplianceLevel.equals("1.8"))
     {
       javaFacetVersion = JavaFacet.VERSION_1_8;
+    }
+    else if (jdkComplianceLevel.equals("1.9"))
+    {
+      javaFacetVersion = JavaFacet.VERSION_1_9;
     }
     else
     {
