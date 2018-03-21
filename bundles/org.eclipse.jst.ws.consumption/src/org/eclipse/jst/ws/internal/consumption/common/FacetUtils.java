5964a15fadf1c2afcaefbd3c8a023734e3179007
Keith Chong <kchong@ca.ibm.com>
2018-03-21 13:23:34 -0400

    [532717] Update Web Services to support Java 10
    Change-Id: If7d7f16b431cdd64daf3700e32d51938fd76dfd1

--- bundles/org.eclipse.jst.ws.consumption/src/org/eclipse/jst/ws/internal/consumption/common/FacetUtils.java
+++ bundles/org.eclipse.jst.ws.consumption/src/org/eclipse/jst/ws/internal/consumption/common/FacetUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2017 IBM Corporation and others.
+ * Copyright (c) 2007, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1035,6 +1035,10 @@ public class FacetUtils
     else if (jdkComplianceLevel.equals("9"))
     {
       javaFacetVersion = JavaFacet.VERSION_9;
+    }
+    else if (jdkComplianceLevel.equals("10"))
+    {
+      javaFacetVersion = JavaFacet.VERSION_10;
     }
     else
     {
