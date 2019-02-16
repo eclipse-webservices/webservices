15e47f416d48ce312d8cd3237afa1801b2b7d8ec
sclarke <shane_clarke@hotmail.com>
2019-02-16 21:49:06 +0000

    [544146] Update copyrights and licenses to EPL 2.0

--- jaxws/bundles/org.eclipse.jst.ws.cxf.creation.ui/src/org/eclipse/jst/ws/internal/cxf/creation/ui/widgets/Java2WSTypeConfigWidget.java
+++ jaxws/bundles/org.eclipse.jst.ws.cxf.creation.ui/src/org/eclipse/jst/ws/internal/cxf/creation/ui/widgets/Java2WSTypeConfigWidget.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2019 Shane Clarke.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Shane Clarke - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.jst.ws.internal.cxf.creation.ui.widgets;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
